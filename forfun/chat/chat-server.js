8eafca6d8d11db35d523c587f9490287edeea8a8
fsp <fsp261@gmail.com>
2013-08-06 15:32:59 +0800

    a simple web chat with nodejs

---
+++ forfun/chat/chat-server.js
@@ -0,0 +1,91 @@
+"use strict";
+
+process.title = 'node-chat';
+
+var webSocketServerPort = 1337;
+var webSocketServer = require('websocket').server;
+var http = require('http');
+
+// global
+var history = [];
+var clients = [];
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+var colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange'];
+colors.sort(function(a, b) { return Math.random() > 0.5; });
+
+// HTTP Server
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' HTTP server. URL' + request.url + ' requested.');
+
+    if (request.url === '/status') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        var responseObject = {
+            currentClients: clients.length,
+            totalHistory: history.length
+        }
+        response.end(JSON.stringify(responseObject));
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end('Sorry, unknown url');
+    }
+});
+server.listen(webSocketServerPort, function() {
+    console.log((new Date()) + " Server is listening on port " + webSocketServerPort);
+});
+
+// WebSocket Server
+var wsServer = new webSocketServer({
+    httpServer: server
+});
+
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    var connection = request.accept(null, request.origin);
+    var index = clients.push(connection) - 1;
+    var userName = false;
+    var userColor = false;
+
+    console.log((new Date()) + ' Connection accepted.');
+    
+    if (history.length > 0) {
+        connection.sendUTF(JSON.stringify( { type: 'history', data: history}));
+    }
+
+    connection.on('message', function(message) {
+        if (message.type == 'utf8') {
+            if (userName === false) {
+                userName = htmlEntities(message.utf8Data);
+                userColor = colors.shift();
+                connection.sendUTF(JSON.stringify({ type: 'color', data: userColor }));
+                console.log((new Date()) + ' User is known as: ' + userName + ' with ' + userColor + ' color.');
+            } else {
+                console.log((new Date()) + ' Received Message from ' + userName + ': ' + message.utf8Data);
+                var obj = {
+                    time: (new Date()).getTime(),
+                    text: htmlEntities(message.utf8Data),
+                    author: userName,
+                    color: userColor
+                };
+                history.push(obj);
+                history = history.slice(-100);
+
+                var json = JSON.stringify({ type: 'message', data: obj });
+                for (var i=0; i < clients.length; i++) {
+                    clients[i].sendUTF(json);
+                }
+            }
+        }
+    });
+
+    connection.on('close', function(connection) {
+        if (userName !== false && userColor !== false) {
+            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+            clients.splice(index, 1);
+            colors.push(userColor);
+        }
+    });
+});

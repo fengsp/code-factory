513caa50f1f3f8c6906d81777dc2bfe07dccf8e8
fsp <fsp261@gmail.com>
2013-09-27 16:07:22 +0800

    add some test recipe

--- tests/cookbook/recipe2.py
+++ tests/cookbook/recipe2.py
@@ -8,5 +8,43 @@ class RomanNumeralConverter(object):
         val = 0
         for char in roman_numeral:
             val += self.digit_map[char]
-         return val
+        return val
 
+
+import unittest
+class RomanNumeralConverterTest(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+        print "Creating a new RomanNumeralConverter..."
+        self.cvt = RomanNumeralConverter()
+
+    def tearDown(self):
+        print "Destroying the RomanNumeralConverter..."
+        self.cvt = None
+
+    def test_parsing_millenia(self):
+        self.assertEquals(1000, self.cvt.convert_to_decimal("M"))
+
+    def test_parsing_century(self):
+        self.assertEquals(100, self.cvt.convert_to_decimal("C"))
+
+    def test_parsing_half_century(self):
+        self.assertEquals(50, self.cvt.convert_to_decimal("L"))
+
+    def test_parsing_half_decade(self):
+        self.assertEquals(5, self.cvt.convert_to_decimal("V"))
+
+    def test_parsing_one(self):
+        self.assertTrue(self.cvt.convert_to_decimal("") == 0)
+
+    def test_empty_roman_numeral(self):
+        self.assertTrue(self.cvt.convert_to_decimal("") == 0)
+        self.assertFalse(self.cvt.convert_to_decimal("") > 0)
+
+    def test_no_roman_numeral(self):
+        self.assertRaises(TypeError, self.cvt.convert_to_decimal, None)
+
+
+if __name__ == "__main__":
+    unittest.main()

2e0960912879ac35ffdde4e2add35aeb6d9100c0
fsp <fsp261@gmail.com>
2013-12-19 17:53:45 +0800

    authentication

---
+++ web/cookies.py
@@ -0,0 +1,56 @@
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+from tornado.options import define, options
+import os.path
+define("port", default=8000, help="port", type=int)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("username")
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        return self.render('login.html')
+    
+    def post(self):
+        self.set_secure_cookie("username", self.get_argument("username"))
+        self.redirect("/")
+
+
+class WelcomeHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render('welcome.html', user=self.current_user)
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        if (self.get_argument("logout", None)):
+            self.clear_cookie("username")
+            self.redirect("/")
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+
+    settings = {
+        "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+        "cookie_secret": "vlW8xdoZRPOK3N71iwnI3OqOD6ArK0dNqONFlfVd3Q8=",
+        "xsrf_cookies": True,
+        "login_url": "/login"
+    }
+
+    application = tornado.web.Application(
+        handlers = [
+            (r'/', WelcomeHandler),
+            (r'/login', LoginHandler),
+            (r'/logout', LogoutHandler)
+        ], **settings)
+
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()

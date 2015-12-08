b2f6b2f8d4a909611895cd68f38f680f38dda370
Shipeng Feng <fsp261@gmail.com>
2015-12-08 13:41:33 +0800

    Finished hello react

--- web/react/app.py
+++ web/react/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request
 
 
 app = Flask(__name__)
+
+
+comments = [
+    {"id": 1, "author": "Pete Hunt", "text": "This is one comment"},
+    {"id": 2, "author": "Jordan Walke", "text": "This is *another*"},
+]
+current_id = 3
 
 
 @app.route('/')
@@ -10,14 +17,22 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/comments')
-def get_comments():
-    comments = [
-        {"id": 1, "author": "Pete Hunt", "text": "This is one comment"},
-        {"id": 2, "author": "Jordan Walke", "text": "This is *another*"},
-    ]
+@app.route('/comments', methods=['GET', 'POST'])
+def handle_comments():
+    global comments
+    global current_id
+    if request.method == 'POST':
+        author = request.form['author']
+        text = request.form['text']
+        comment = {
+            'id': current_id,
+            'author': author,
+            'text': text,
+        }
+        comments.append(comment)
+        current_id += 1
     return jsonify(comments=comments)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
